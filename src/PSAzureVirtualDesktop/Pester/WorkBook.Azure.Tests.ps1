02c2086d724505c5a7242dfc0293534a09e246dd
Laurent VAN ACKER <laurent.vanacker@free.fr>
2024-11-11 09:35:31 +0100

    Minor update

--- src/PSAzureVirtualDesktop/Pester/WorkBook.Azure.Tests.ps1
+++ src/PSAzureVirtualDesktop/Pester/WorkBook.Azure.Tests.ps1
@@ -7,7 +7,7 @@ Describe "Azure Virtual Desktop Workbooks"{
     Context "'<_>' Workbook" -ForEach $WorkBook.Keys {
         It  "'<_>' Workbook exists" {
             $DisplayName = $_
-            $(Get-AzApplicationInsightsWorkbook -Category workbook | Where-Object -FilterScript { $_.DisplayName -eq $DisplayName}) | Should -Not -BeNullOrEmpty -ErrorAction Stop
+            $(Get-AzApplicationInsightsWorkbook -Category workbook | Where-Object -FilterScript { $_.DisplayName -eq $DisplayName}) | Should -Not -BeNullOrEmpty #-ErrorAction Stop
         }
     }
 }
