b128e8d1215bfc01b6a2d3ec5cba9befaa0c7e24
Laurent VAN ACKER <laurent.vanacker@free.fr>
2024-12-19 14:34:01 +0100

    Minor changes

--- src/PSAzureVirtualDesktop/ScriptsToProcess/Update-AzDesktopVirtualizationModule.ps1
+++ src/PSAzureVirtualDesktop/ScriptsToProcess/Update-AzDesktopVirtualizationModule.ps1
@@ -2,13 +2,13 @@
 function Update-AzDesktopVirtualizationModule {
     [CmdletBinding()]
     Param (
-        [string] $MinimumVersion = "5.2.1"
+        [string] $MaximumVersion = "5.3.0"
     )
     
     $HighestInstalledVersion = Get-Module -Name Az.DesktopVirtualization -ListAvailable | Sort-Object -Property Version -Descending | Select-Object -First 1
     Write-Verbose "`$HighestInstalledVersion:`r`n$($HighestInstalledVersion | Out-String)"
-    if ($HighestInstalledVersion.Version -lt [system.version]::parse($MinimumVersion)) {
-        $InstalledModule = Install-Module -Name Az.DesktopVirtualization -Force -MinimumVersion $MinimumVersion -AllowPrerelease -PassThru
+    if ($HighestInstalledVersion.Version -lt [system.version]::parse($MaximumVersion)) {
+        $InstalledModule = Install-Module -Name Az.DesktopVirtualization -Force -MaximumVersion $MaximumVersion -AllowPrerelease -PassThru
         Write-Verbose "Installed Version:`r`n$($InstalledModule | Out-String)"
     }
 }
